9dbdff710db4cbbb77eb32c062d93628801b073f
gaara <realwol@gmail.com>
2015-12-09 15:59:18 +0800

    fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  namesapce :v1 do
+  namespace :v1 do
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
