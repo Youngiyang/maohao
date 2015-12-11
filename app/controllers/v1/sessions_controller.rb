f927dbc1656650aadf7e16bcf782c0a0d15ae0ac
FoxZhao <395381480@qq.com>
2015-12-11 13:28:40 +0800

    完成登录功能

---
+++ app/controllers/v1/sessions_controller.rb
@@ -0,0 +1,18 @@
+class V1::SessionsController < V1::BaseController
+  def create
+    status, code, return_hash = true, "000", {}
+    @user = User.find_by(mobile: session_params[:mobile])
+    if @user && @user.authenticate(session_params[:password])
+      return_hash[:auth_token] = @user.auth_token
+      # return_hash[:user_link] = v1_user_url(@user)
+    else
+      status = false
+      code = "101001"
+    end
+    render json: api_return(status, code, return_hash)
+  end
+
+  def session_params
+    params.require(:session).permit(:mobile, :password)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   has_many :collections
   has_many :shops
 
+  has_secure_password validations: false
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, format: { with: VALID_EMAIL_REGEX }, uniqueness: true, allow_blank: true
   VALID_MOBILE_REGEX = /\A(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}\z/
