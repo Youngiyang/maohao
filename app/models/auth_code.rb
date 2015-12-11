2dd6963b02ce204b269143b4521909270e8b2e51
liujun <lj90214@gmail.com>
2015-12-11 12:05:12 +0800

    make previous auth code invalidate

--- app/models/auth_code.rb
+++ app/models/auth_code.rb
@@ -3,8 +3,7 @@ class AuthCode < ActiveRecord::Base
   VALID_MOBILE_REGEX = /\A(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}\z/
   validates :mobile, presence: true, format: { with: VALID_MOBILE_REGEX }
   validates :code, presence: true
-
-  before_validation :set_code
+  validates :auth_code_type, inclusion: {in: %w(sign_up forget_password)}
 
   def self.generate_code
     ((0...9).to_a.sample(3) + (0...9).to_a.sample(3)).join()
@@ -17,12 +16,4 @@ class AuthCode < ActiveRecord::Base
   def send_frequently?
     sent_at < 1.minutes.ago
   end
-
-  private
-    def set_code
-      unless auth_state
-        self.code = AuthCode.generate_code
-        self.expire_at = Time.now + 10.minutes
-      end
-    end
 end
