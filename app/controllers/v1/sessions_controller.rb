f293ff7b476ab21e8e53dcba917abc1f309b3d06
FoxZhao <395381480@qq.com>
2015-12-14 14:34:58 +0800

    修改sessioncontroller中status的默认值为false

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -1,17 +1,16 @@
 class V1::SessionsController < V1::BaseController
   def create
-    status, code, return_hash = true, "000", {}
+    status, code, return_hash = false, "000", {}
     @user = User.find_by(mobile: session_params[:mobile])
     if @user.state == 1
       if @user && @user.authenticate(session_params[:password])
+        status = true
         return_hash[:auth_token] = @user.auth_token
         # return_hash[:user_link] = v1_user_url(@user)
       else
-        status = false
         code = "101001"
       end
     else
-      status = false
       code = "101002"
     end
       render json: api_return(status, code, return_hash)
