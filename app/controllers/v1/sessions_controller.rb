777247a7c21f7026a8e89ad7d60590878636c034
FoxZhao <395381480@qq.com>
2015-12-14 15:32:10 +0800

    修改参数传入方式

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -1,9 +1,9 @@
 class V1::SessionsController < V1::BaseController
   def create
     status, code, return_hash = false, "000", {}
-    @user = User.find_by(mobile: session_params[:mobile])
+    @user = User.find_by(mobile: params[:mobile])
     if @user.state == 1
-      if @user && @user.authenticate(session_params[:password])
+      if @user && @user.authenticate(params[:password])
         status = true
         return_hash[:auth_token] = @user.auth_token
         # return_hash[:user_link] = v1_user_url(@user)
@@ -16,7 +16,7 @@ class V1::SessionsController < V1::BaseController
       render json: api_return(status, code, return_hash)
   end
 
-  def session_params
-    params.require(:session).permit(:mobile, :password)
-  end
+  # def session_params
+  #   params.require().permit(:mobile, :password)
+  # end
 end
