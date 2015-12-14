2c9a337b7d6116ef8acdf0349e2c565d617d84b0
FoxZhao <395381480@qq.com>
2015-12-14 18:20:39 +0800

    修改账号未注册时登录会报错的问题

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -2,16 +2,16 @@ class V1::SessionsController < V1::BaseController
   def create
     status, code, return_hash = false, "000", {}
     @user = User.find_by(mobile: params[:mobile])
-    if @user.state == 1
-      if @user && @user.authenticate(params[:password])
+    if @user && @user.authenticate(params[:password])
+      if @user.state == 1
         status = true
         return_hash[:auth_token] = @user.auth_token
         # return_hash[:user_link] = v1_user_url(@user)
       else
-        code = "101001"
+        code = "101002"
       end
     else
-      code = "101002"
+      code = "101001"
     end
     render json: api_return(status, code, return_hash)
   end
