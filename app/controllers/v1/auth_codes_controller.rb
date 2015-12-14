5553eeed4a2770eca0e70120c59be6048d001a41
liujun <lj90214@gmail.com>
2015-12-14 22:07:43 +0800

    add parameter present validations

--- app/controllers/v1/auth_codes_controller.rb
+++ app/controllers/v1/auth_codes_controller.rb
@@ -1,40 +1,53 @@
 class V1::AuthCodesController < V1::BaseController
   def create
     status, code, return_hash = false, '000', {}
-
     auth_code_type = params[:auth_code_type]
 
-    auth_code = AuthCode.new(
-      mobile: params[:mobile],
-      auth_code_type: auth_code_type,
-      auth_state: false,
-      code: AuthCode.generate_code,
-      expire_at: Time.now + 10.minutes
-    )
+    if params[:mobile].present? && params[:auth_code_type].present?
+      auth_code = AuthCode.new(
+        mobile: params[:mobile],
+        auth_code_type: auth_code_type,
+        auth_state: false,
+        code: AuthCode.generate_code,
+        expire_at: Time.now + 10.minutes
+      )
 
-    # 把相同手机号码且相同类型的作废
-    if auth_code.valid?
-      AuthCode.where(mobile: params[:mobile], auth_code_type: auth_code_type, auth_state: false).update_all(auth_state: true)
-    end
-
-    if auth_code.save
-      if SmsHelper.send_auth_code_sms params[:mobile], auth_code.code, auth_code_type
-        auth_code.update_attribute(:sent_at, Time.now)
-        status = true
-        return_hash[:message] = '手机验证码发送成功'
-      else
-        code = '100002'
-      end
-    else
-      if auth_code.errors.include?(:mobile)
-        code = '100001'
-      elsif auth_code.errors.include?(:auth_code_type)
-        code = '100004'
+      if auth_code.valid?
+        # 把相同手机号码且相同类型的作废
+        AuthCode.deactivate_old_auth_code(params[:mobile], params[:auth_code_type])
+        result =  check_auth_type_for_mobile(params[:mobile], params[:auth_code_type])
+        if result.nil?
+          auth_code.save(validate: false)
+          if SmsHelper.send_auth_code_sms params[:mobile], auth_code.code, auth_code_type
+            auth_code.update_attribute(:sent_at, Time.now)
+            status = true
+            return_hash[:message] = '手机验证码发送成功'
+          else
+            code = '100002'
+          end
+        else
+          code = result
+        end
       else
         code = '100003'
+        return_hash[:errors] = auth_code.errors
       end
+    else
+      code = '100001'
     end
-
     render json: api_return(status, code, return_hash)
   end
+
+  private
+    def check_auth_type_for_mobile(mobile, auth_code_type)
+      if auth_code_type == 'sign_up'
+        if User.exists?(mobile: mobile)
+          '100004'
+        end
+      elsif auth_code_type == 'forget_password'
+        unless User.exists?(mobile: mobile)
+          '100005'
+        end
+      end
+    end
 end
--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,24 +1,30 @@
 class V1::UsersController < V1::BaseController
   def create
     status, code, return_hash = false, "000", {}
-    if User.exists?(mobile: params[:mobile])
-      code = 102001
-    else
-      if AuthCode.valid_auth_code?(params[:mobile], 'sign_up', params[:auth_code])
-        user = User.new(
-          mobile: params[:mobile],
-          password: params[:password]
-          auth_token: SecureRandom.uuid
-        )
-        if user.save
-          status = true
-          return_hash[:message] = '注册成功'
-        else
-          code = 102002
-        end
+    if params[:mobile].present? && params[:password].present?
+      if User.exists?(mobile: params[:mobile])
+        code = 102001
       else
-        code = 102003
+        if AuthCode.valid_auth_code?(params[:mobile], 'sign_up', params[:auth_code])
+          user = User.new(
+            mobile: params[:mobile],
+            password: params[:password],
+            auth_token: SecureRandom.uuid
+          )
+          if user.save
+            status = true
+            return_hash[:message] = '注册成功'
+          else
+            # 参数校验错误
+            code = 102002
+          end
+        else
+          # 验证码错误
+          code = 102003
+        end
       end
+    else
+      code = 102004
     end
 
     render json: api_return(status, code, return_hash)
