de31a71688321a059c7ecdc935047a176f1ee80b
FoxZhao <395381480@qq.com>
2015-12-15 10:38:08 +0800

    添加参数是否为空的验证

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -2,19 +2,23 @@ class V1::SessionsController < V1::BaseController
   def create
     status, code, return_hash = false, "000", {}
     @user = User.find_by(mobile: params[:mobile])
-    if @user
-      if @user.authenticate(params[:password])
-        if @user.state == 1
-          status = true
-          return_hash[:auth_token] = @user.auth_token
+    if params[:mobile].present? && params[:password].present?
+      if @user
+        if @user.authenticate(params[:password])
+          if @user.state == 1
+            status = true
+            return_hash[:auth_token] = @user.auth_token
+          else
+            code = "101002"
+          end
         else
-          code = "101002"
+          code = "101001"
         end
       else
-        code = "101001"
+        code = "101003"
       end
     else
-      code = "101003"
+      code = "101004"
     end
     render json: api_return(status, code, return_hash)
   end
