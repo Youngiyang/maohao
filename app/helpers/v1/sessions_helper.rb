964a01114db67e13d557f6b6d663a31a7f4f08d7
FoxZhao <395381480@qq.com>
2015-12-16 21:49:26 +0800

    修改session_helper中认证用户的方法，增加参数判空 修改current_user方法，只能以参数形式传auth_token

--- app/helpers/v1/sessions_helper.rb
+++ app/helpers/v1/sessions_helper.rb
@@ -3,21 +3,26 @@ module V1::SessionsHelper
 
   def authenticated_by_token!
     status, code, return_hash = false, "000", {}
-    if current_user
-      unless current_user.state == 1
-        code = "101002"
+    if params[:auth_token].present?
+      if current_user
+        unless current_user.state == 1
+          # 账号已被禁用
+          code = "101002"
+          render json: api_return(status, code, return_hash)
+        end
+      else
+        # 不存在与authen_token匹配的用户
+        code = "101005"
         render json: api_return(status, code, return_hash)
       end
     else
-      code = "101005"
+      # 参数缺失
+      code = "103004"
       render json: api_return(status, code, return_hash)
     end
   end
 
   def current_user
-    token = ActionController::HttpAuthentication::Token.token_and_options(request)[0]
-    if token
-      @current_user ||= User.find_by(auth_token: token)
-    end
+    @current_user ||= User.find_by(auth_token: params[:auth_token])
   end
 end
