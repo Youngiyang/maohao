d2147951843e91a37fdbb3d1134e42d3b990dac8
FoxZhao <395381480@qq.com>
2015-12-16 15:36:59 +0800

    增加新密码与原密码是否一样的判断 将赋值语句放到判空之后

--- app/controllers/v1/user_controller.rb
+++ app/controllers/v1/user_controller.rb
@@ -3,22 +3,27 @@ class V1::UserController < V1::BaseController
 
   def reset_password
     status, code, return_hash = false, "000", {}
-    old_password = params[:old_password]
-    new_password = params[:new_password]
-    new_password_confirmation = params[:new_password_confirmation]
-    if new_password.present? && new_password_confirmation.present? && old_password.present?
+    if params[:old_password].present? && params[:new_password].present? && params[:new_password_confirmation].present?
+      old_password = params[:old_password]
+      new_password = params[:new_password]
+      new_password_confirmation = params[:new_password_confirmation]
       if current_user.authenticate(old_password)
         if new_password == new_password_confirmation
-          current_user.password = new_password
-          current_user.reset_auth_token
-          if current_user.save
-            status = true
-            return_hash[:message] = "密码已修改"
-            return_hash[:auth_token] = current_user.auth_token
+          if new_password != old_password
+            current_user.password = new_password
+            current_user.reset_auth_token
+            if current_user.save
+              status = true
+              return_hash[:message] = "密码已修改"
+              return_hash[:auth_token] = current_user.auth_token
+            else
+              # 新密码格式有误
+              code = "103003"
+              return_hash[:errors] = current_user.errors
+            end
           else
-            # 新密码格式有误
-            code = "103003"
-            return_hash[:errors] = current_user.errors
+            # 新密码与原密码一样
+            code = "103005"
           end
         else
           # 确认密码与新密码不一致
