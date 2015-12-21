8ff9f12f82334474c50e19d12e324376971d6f03
liujun <lj90214@gmail.com>
2015-12-21 15:23:56 +0800

    remove original controller, helper and test

--- app/controllers/v1/errors_controller.rb
+++
@@ -1,5 +0,0 @@
-class V1::ErrorsController < V1::BaseController
-  def index
-    render json: api_return(false, "102")
-  end
-end
--- test/controllers/v1/errors_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class V1::ErrorsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
