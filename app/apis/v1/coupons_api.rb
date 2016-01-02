e8b9b17f652c145a0b9f403587cb0cda930c65c0
gaara <realwol@gmail.com>
2016-01-02 22:48:17 +0800

    create coupon_item redundancy

--- app/apis/v1/coupons_api.rb
+++ app/apis/v1/coupons_api.rb
@@ -10,6 +10,7 @@ module V1
         authenticate_by_token!
         coupon = Coupon.find(params[:id])
         if coupon.is_coupon_grab_time? && current_user.is_coupon_out_of_limit?(coupon)
+          current_user.save_coupon_item_redundancy coupon
           coupon.increment!(:giveout)
           present coupon, with: CouponDetailWithShopEntity
         else
