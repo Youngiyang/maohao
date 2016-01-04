197e2ec71772123e2e71e8a3f446e45dcf8cbc24
youngi <youngiyang@163.com>
2016-01-04 10:12:27 +0800

    pull分类

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151210095803) do
+ActiveRecord::Schema.define(version: 20151215023116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "postgis"
 
+  create_table "admins", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.integer  "group_id",        default: 1,     null: false
+    t.string   "password_digest",                 null: false
+    t.boolean  "is_super",        default: false, null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.string   "remember_digest"
+  end
+
   create_table "auth_codes", force: :cascade do |t|
-    t.string   "mobile",                         null: false
-    t.string   "code",                           null: false
-    t.boolean  "auth_state",     default: false, null: false
-    t.integer  "validated_time", default: 0,     null: false
+    t.string   "mobile",                     null: false
+    t.string   "code",                       null: false
+    t.boolean  "auth_state",                 null: false
+    t.integer  "validated_time", default: 0, null: false
     t.datetime "sent_at"
-    t.datetime "expire_at",                      null: false
+    t.datetime "expire_at",                  null: false
     t.string   "auth_code_type"
   end
 
@@ -123,7 +134,7 @@ ActiveRecord::Schema.define(version: 20151210095803) do
     t.boolean   "business_on_holiday",                                                          default: true,  null: false
     t.float     "star_grade",                                                                   default: 5.0,   null: false
     t.integer   "user_id",                                                                                      null: false
-    t.boolean   "is_recommand",                                                                 default: false, null: false
+    t.boolean   "is_recommand",                                                                 default: true,  null: false
     t.text      "description",                                                                  default: "",    null: false
     t.boolean   "is_own",                                                                       default: false, null: false
     t.text      "notice",                                                                       default: "",    null: false
@@ -157,6 +168,7 @@ ActiveRecord::Schema.define(version: 20151210095803) do
     t.integer  "state",           default: 1,     null: false
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
+    t.string   "remember_digest"
   end
 
   add_index "users", ["auth_token"], name: "index_users_on_auth_token", unique: true, using: :btree
