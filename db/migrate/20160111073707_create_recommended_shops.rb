aa407a9620e97b52ec7a11c84c06097a59b4736c
FoxZhao <395381480@qq.com>
2016-01-11 16:01:36 +0800

    add state to recommend_shops

--- db/migrate/20160111073707_create_recommended_shops.rb
+++ db/migrate/20160111073707_create_recommended_shops.rb
@@ -3,6 +3,8 @@ class CreateRecommendedShops < ActiveRecord::Migration
     create_table :recommended_shops do |t|
       t.integer :shop_id, null: false
       t.string :image, null: false
+      # 1为推荐，2为取消推荐
+      t.integer :state, null: false, default: 1
       t.timestamps null: false
     end
   end
