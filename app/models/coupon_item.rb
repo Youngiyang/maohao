3ea3e3482df7d72ab9a5c9621ed5253628957520
FoxZhao <395381480@qq.com>
2016-01-14 11:30:30 +0800

    修改coupon_sn生成的方式

--- app/models/coupon_item.rb
+++ app/models/coupon_item.rb
@@ -4,4 +4,13 @@ class CouponItem < ActiveRecord::Base
 
   scope :un_used, ->{where(state: 0)}
 
+  def self.get_coupon_sn
+    loop do
+      nums = (0..9).to_a
+      prefix = Time.now.strftime('%y%m%d')
+      postfix = nums.sample(6).join
+      coupon_sn = prefix + postfix
+      return coupon_sn unless CouponItem.exists?(coupon_sn: coupon_sn)
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -58,7 +58,7 @@ class User < ActiveRecord::Base
   end
 
   def save_coupon_item_redundancy coupon
-    self.coupon_items.create(coupon_id: coupon.id, coupon_sn: SecureRandom.uuid, state: 0,
+    self.coupon_items.create(coupon_id: coupon.id, coupon_sn: CouponItem.get_coupon_sn, state: 0,
                              expired_at: coupon.end_time, shop_id: coupon.shop_id,
                              shop_name: coupon.shop.name, coupon_name: coupon.name, coupon_type: coupon.cc_type,
                              coupon_cheap: coupon.cheap, coupon_discount: coupon.discount,
