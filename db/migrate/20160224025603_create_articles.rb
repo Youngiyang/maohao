b860c1c615c50953c71320952f1fba6789a7d500
youngi <youngiyang@163.com>
2016-02-25 11:04:36 +0800

    修改文章模型

--- db/migrate/20160224025603_create_articles.rb
+++ db/migrate/20160224025603_create_articles.rb
@@ -4,10 +4,10 @@ class CreateArticles < ActiveRecord::Migration
       t.string :title, null: false
       # 1为正常，2为草稿，3为回收
       t.integer :state, null: false, default: 1
-      t.string :content, null: false
+      t.text :content, null: false
       t.integer :order, null: false
       t.string :category, null: false
-      t.string :author, null: false
+      t.integer :author_id, null: false
       t.string :excerpt
       t.timestamps null: false
     end
--- db/migrate/20160224031618_create_article_categories.rb
+++ db/migrate/20160224031618_create_article_categories.rb
@@ -1,6 +1,6 @@
 class CreateArticleCategories < ActiveRecord::Migration
   def change
-    create_table :article_categories do |t
+    create_table :article_categories do |t|
       t.string :name, null: false
       t.string :description
       t.integer :count
